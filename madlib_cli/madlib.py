b6a03ba42a92243bcc761737dc08082c11754c85
Brendon <Brendonhampton1988@gmail.com>
2022-05-21 21:57:30 -0700

    cant figure out what order this should be in so my tests are not working at all

--- madlib_cli/madlib.py
+++ madlib_cli/madlib.py
@@ -1,3 +1,4 @@
+import re
 story_file = '../assets/dark_and_stormy_night_template.txt'
 
 intro = """****Welcome to the Madlib Game!****
@@ -23,25 +24,26 @@ def read_template(file):
 def parse_template(file):
     try:
         list = read_template(file)
-        return list
+        newlist = re.findall(r'\{.*?\}', list)
+        print(newlist)
+        # return list, newlist
     except AssertionError as e:  
         print(e)
 # create merge function
 def merge(file):
-    try:
-        list = parse_template(file)
-        adj_One = input('Enter Adjective : ')
-        adj_Two = input('Enter Second Adjective : ')
-        noun = input('Enter a noun : ')
-        print(adj_One,adj_Two,noun)
-        list = list.replace("{Adjective}", adj_One, 1)
-        list = list.replace("{Adjective}", adj_Two, 2)
-        list = list.replace("{Noun}", noun, 3)
-        print(list)
-        merge(story_file)
+    pass
+    # try:
+    #     list = parse_template(file)
+    #     adj_One = input('Enter Adjective : ')
+    #     adj_Two = input('Enter Second Adjective : ')
+    #     noun = input('Enter a noun : ')
+    #     print(adj_One,adj_Two,noun)
+    #     print(list)
+    #     # merge(story_file)
 
-    except AssertionError as e:
-        print(e)
+    # except AssertionError as e:
+    #     print(e)
 
-
-merge(story_file)
+read_template(story_file)
+# parse_template(story_file)
+# merge(story_file)
