e25b223c354cf1f07f29560c475ec7505e0ced63
Brendon <Brendonhampton1988@gmail.com>
2022-05-28 16:22:18 -0700

    got 3rd test to finalkly work!!! took me 6 hours but i got it!

--- test_madlib.py
+++ test_madlib.py
@@ -3,7 +3,7 @@ from madlib_cli.madlib import read_template, parse_template, merge
 
 
 def test_read_template_returns_stripped_string():
-    actual = read_template("assets/template.txt")
+    actual = read_template("assets/dark_and_stormy_night_template.txt")
     expected = "It was a {Adjective} and {Adjective} {Noun}."
     assert actual == expected
 
@@ -27,7 +27,7 @@ def test_merge():
     assert actual == expected
 
 
-@pytest.mark.skip("pending")
+# @pytest.mark.skip("pending")
 def test_read_template_raises_exception_with_bad_path():
 
     with pytest.raises(FileNotFoundError):
